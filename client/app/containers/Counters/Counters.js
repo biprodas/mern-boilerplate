3e45fb5627db9085d0a516fc7134cb215fbebdc9
Keith Weaver <keithmweaver@gmail.com>
2018-02-16 11:20:05 -0500

    Added counters container to better demo redux calls

---
+++ client/app/containers/Counters/Counters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Counters = () => (
+  <div>
+    <h1>Counters</h1>
+  </div>
+);
+
+export default Counters;
--- client/app/index.js
+++ client/app/index.js
@@ -19,6 +19,7 @@ import App from './containers/App/App';
 import NotFound from './containers/App/NotFound';
 import Home from './containers/Home/Home';
 import HelloWorld from './containers/HelloWorld/HelloWorld';
+import Counters from './containers/Counters/Counters';
 
 // Styles
 import './styles/styles.scss';
@@ -38,8 +39,9 @@ render((
     <Router>
       <App>
         <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route path="/helloworld" component={HelloWorld}/>
+          <Route exact path="/" component={Home} />
+          <Route path="/counters" component={Counters} />
+          <Route path="/helloworld" component={HelloWorld} />
           <Route component={NotFound}/>
         </Switch>
       </App>
