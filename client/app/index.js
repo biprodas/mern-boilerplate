8bc7586aeabf2725012617a86b939e796243b0a0
Keith Weaver <keithmweaver@gmail.com>
2018-02-16 13:18:38 -0500

    Exported store to a new file

--- client/app/index.js
+++ client/app/index.js
@@ -10,12 +10,8 @@ import {
 } from 'react-router-dom';
 
 // Redux related
-import { createStore, applyMiddleware } from 'redux';
-import allReducers from './reducers';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import promise from 'redux-promise-middleware';
+import store from './store';
 
 // Containers
 import App from './containers/App/App';
@@ -26,15 +22,6 @@ import Counters from './containers/Counters/Counters';
 
 // Styles
 import './styles/styles.scss';
-
-// Redux
-// Store is all this application data.
-const logger = createLogger({});
-const store = createStore(allReducers, {}, applyMiddleware(logger, thunk, promise()));
-// Reducer is a function that tells what data to store in store.
-//         They take an action adn update part of the application
-//         state. Reducers are broken down by parts.
-// Provider makes your store/data available to the containers.
 
 render((
   <Provider
