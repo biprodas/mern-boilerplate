bd41881b4b6c3d71f1a36441edd02eedb0af3c8c
Keith Weaver <keithmweaver@gmail.com>
2018-02-16 11:46:57 -0500

    Wrote containers to grab store data

--- client/app/containers/Counters/Counters.js
+++ client/app/containers/Counters/Counters.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-const Counters = () => (
-  <div>
-    <h1>Counters</h1>
-  </div>
-);
+class Counters extends Component {
+  constructor() {
+    super();
 
-export default Counters;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Counters</h1>
+        {
+          (this.props.counters) ? (
+            (this.props.counters).map(counter => {
+              return (
+                <li key={Math.random()}>{counter._id} - {counter.count}</li>
+              )
+            })
+          ) : (null)
+        }
+      </div>
+    );
+  }
+}
+
+// Takes the application store (main data) and passes into
+// your container as a prop. It can pass any aspect of the
+// store. So we want counters, so state.counters. We can
+// now reference this.props.counters to grab counters in
+// store.
+function mapStateToProps(state) {
+  return {
+    counters: state.counters
+  };
+};
+
+export default connect(mapStateToProps)(Counters);
