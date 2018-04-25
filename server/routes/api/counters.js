6ac4c9a4ace98a71f8646d4758cd84500a3d8565
Keith Weaver <keithmweaver@gmail.com>
2018-04-24 22:20:40 -0400

    Added AWS Dynamo

--- server/routes/api/counters.js
+++ server/routes/api/counters.js
@@ -1,51 +1,112 @@
-const Counter = require('../../models/Counter');
+const AWS = require('aws-sdk');
+const config = require('../../../config/config.js');
+
+// More docs: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/SQLtoNoSQL.html
 
 module.exports = (app) => {
   app.get('/api/counters', (req, res, next) => {
-    Counter.find()
-      .exec()
-      .then((counter) => res.json(counter))
-      .catch((err) => next(err));
+    AWS.config.update({
+      region: config.aws_region
+    });
+
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    var params = {
+      TableName: config.aws_dynamodb_table
+    };
+
+    docClient.scan(params, function(err, data) {
+      if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+      } else {
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+      }
+    });
   });
 
   app.post('/api/counters', function (req, res, next) {
-    const counter = new Counter();
+    const counter = {
+      countVal: 0,
+      id: parseInt(Math.random() * 1000)
+    };
 
-    counter.save()
-      .then(() => res.json(counter))
-      .catch((err) => next(err));
+    console.log('counter', counter);
+    console.log('config', config);
+
+    AWS.config.update({
+      region: config.aws_region
+    });
+
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    var params = {
+      TableName: config.aws_dynamodb_table,
+      Item: counter
+    };
+
+    docClient.put(params, function(err, data) {
+      if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+      } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+      }
+    });
   });
 
   app.delete('/api/counters/:id', function (req, res, next) {
-    Counter.findOneAndRemove({ _id: req.params.id })
-      .exec()
-      .then((counter) => res.json())
-      .catch((err) => next(err));
+    const id = parseInt(req.params.id);
+
+    AWS.config.update({
+      region: config.aws_region
+    });
+
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    var params = {
+      TableName: config.aws_dynamodb_table,
+      Key: {
+        'id': id
+      }
+    };
+
+    docClient.delete(params, function(err, data) {
+      if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+      } else {
+        console.log("delete succeeded:", JSON.stringify(data, null, 2));
+      }
+    });
   });
 
   app.put('/api/counters/:id/increment', (req, res, next) => {
-    Counter.findById(req.params.id)
-      .exec()
-      .then((counter) => {
-        counter.count++;
+    const id = parseInt(req.params.id);
 
-        counter.save()
-          .then(() => res.json(counter))
-          .catch((err) => next(err));
-      })
-      .catch((err) => next(err));
-  });
+    AWS.config.update({
+      region: config.aws_region
+    });
 
-  app.put('/api/counters/:id/decrement', (req, res, next) => {
-    Counter.findById(req.params.id)
-      .exec()
-      .then((counter) => {
-        counter.count--;
+    const docClient = new AWS.DynamoDB.DocumentClient();
 
-        counter.save()
-          .then(() => res.json(counter))
-          .catch((err) => next(err));
-      })
-      .catch((err) => next(err));
+    var params = {
+      TableName: config.aws_dynamodb_table,
+      Key: {
+        'id': id
+      },
+      UpdateExpression: "set countVal = :c + countVal",
+      ExpressionAttributeValues: {
+        ':c': 1
+      },
+      ReturnValues: "UPDATED_NEW"
+    };
+
+    docClient.update(params, function(err, data) {
+      if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+      } else {
+        console.log("update succeeded:", JSON.stringify(data, null, 2));
+      }
+    });
+
+    // More info: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.NodeJs.03.html
   });
 };
